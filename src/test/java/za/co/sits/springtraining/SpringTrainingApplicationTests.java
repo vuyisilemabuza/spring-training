c0402751269bcf88ddfeaa174b07e57e358027c9
Vuyisile Mabuza <vuyisilemabuza@gmail.com>
2023-01-06 22:13:18 +0200

    Initial commit

---
+++ src/test/java/za/co/sits/springtraining/SpringTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.sits.springtraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
